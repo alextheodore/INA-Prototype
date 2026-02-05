bd0267770c6b6c03df2fe08cbb22f7edcb435c69
alextheodore <alex.theodore26@gmail.com>
2026-02-05 13:07:49 +0700

    Deployment pertama INA Prototype

---
+++ models_api.py
@@ -0,0 +1,17 @@
+from langchain.chat_models import ChatOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(
+ base_url="https://openrouter.ai/api/v1",
+ api_key=os.getenv("OPENROUTER_API_KEY"), 
+ model="tngtech/deepseek-r1t2-chimera:free",
+ model_kwargs={
+        "extra_headers": {
+            "HTTP-Referer": "http://localhost:8501", 
+            "X-Title": "INA Prototype"
+        }
+    }
+)
